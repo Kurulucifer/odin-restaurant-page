ee6d3765c968f794b9b8c4df563e02aea622d692
Kurulucifer <28943130+Kurulucifer@users.noreply.github.com>
2026-06-17 22:15:45 -0700

    Polish tabs and appearance
    Probably done for now? Maybe come back later but
    I really should've designed the layout first lol

--- src/about.js
+++ src/about.js
@@ -19,7 +19,7 @@ function createAbout() {
     const founderName = document.createElement("h1");
     founderName.textContent = "Eugene Harold Krabs";
     const founderDesc = document.createElement("div");
-    founderDesc.textContent = "hi I'm mr. krabs!!!"
+    founderDesc.textContent = "Eugene Harold Krabs, most widely known as Mr. Krabs, is owner and founder of the Krusty Krab. He invented the legendary Krabby Patty that is beloved by all residents of Bikini Bottom. A former Navy cadet, Mr. Krabs renovated a retirement home into the premier culinary institution that is the Krusty Krab today. He has one daughter, Pearl Krabs."
     founder.appendChild(founderImg);
     founder.appendChild(founderName);
     founder.appendChild(founderDesc);
@@ -29,7 +29,7 @@ function createAbout() {
 
     const squidward = createWorkerItem(
         "Squidward Tentacles",
-        "hi I'm squidward!!!",
+        "Squidward Tentacles is the sole cashier of the Krusty Krabs, efficiently handling all orders call-in or walk-in. Don't let his attitude fool you; he strives to complete every order with speed to satisfy as many customers as possible before the day's end.",
         squidwardImg,
         "A picture of Squidward Tentacles, cashier.",
     );
@@ -37,7 +37,7 @@ function createAbout() {
 
     const spongebob = createWorkerItem(
         "Spongebob Squarepants",
-        "hi I'm spongebob!!!",
+        "Spongebob Squarepants is the sole fry cook, or well, sole cook, of the Krusty Krabs. He is responsible for all meals coming out of the kitchen, and is responsible for all the customers' satisfied smiles.",
         spongebobImg,
         "A picture of Spongebob Squarepants, fry cook.",
     );
@@ -77,25 +77,3 @@ function createWorkerItem(name, desc, img, alt) {
 }
 
 export default createAbout;
-
-// <!-- <div class="founder">
-//       <img src="./images/Eugene_Krabs.png" alt="A picture of Eugene Krabs, owner and founder.">
-//       <h1>Eugene Harold Krabs</h1>
-//       <div>hi I'm mr krabs!!!</div>
-//     </div>
-//     <div class="worker-items">
-//       <div class="worker-item squidward">
-//         <img src="./images/Squidward_Tentacles.png" alt="A picture of Squidward Tentacles, cashier.">
-//         <div>
-//           <h1>Squidward Tentacles</h1>
-//           <div>hi I'm squidward!!!</div>
-//         </div>
-//       </div>
-//       <div class="worker-item spongebob">
-//         <img src="./images/Spongebob_Squarepants.png" alt="A picture of Spongebob Squarepants, fry cook.">
-//         <div>
-//           <h1>Spongebob Squarepants</h1>
-//           <div>hi spongebob!!!</div>
-//         </div>
-//       </div>
-//     </div> -->
--- src/home.js
+++ src/home.js
@@ -1,25 +1,66 @@
-import splashImage from "./images/The_Krusty_Krab.png"
+import splash from "./images/The_Krusty_Krab.png"
+import interior from "./images/Krusty_Krab_In.png";
+import burger from "./images/Krabby_Patty_Splash.png"
+import crew from "./images/Krusty_Crew.png"
 
 function createHome() {
     const home = document.createElement("div");
     home.className = "home";
-    
-    const splash = document.createElement("img");
-    splash.className = "splash";
-    splash.src = splashImage;
-    splash.height = "500";
-    splash.width = "750";
-    splash.alt = "The Krusty Krab";
 
-    const header = document.createElement("h1");
-    header.textContent = "The Krusty Krab";
+    const title = document.createElement("div");
+    title.className = "home-title";
+    title.textContent = "The Krusty Krab";
     
-    const bodyText = document.createElement("div");
-    bodyText.textContent = "The Krusty Krab is world-renowned Michelin 3-star restaurant operating just off the coast of Santa Monica Pier.";
+    const splashImg = document.createElement("img");
+    splashImg.className = "home-image";
+    splashImg.src = splash;
+    splashImg.height = "500";
+    splashImg.width = "750";
+    splashImg.alt = "An image of the Krusty Krab";
+    
+    const introText = document.createElement("div");
+    introText.className = "home-text";
+    introText.textContent = "The Krusty Krab is nation-renowned Michelin 3-star restaurant operating just off the coast of Santa Monica Pier.";
 
-    home.appendChild(header);
-    home.appendChild(splash)
-    home.appendChild(bodyText);
+    const interiorImg = document.createElement("img");
+    interiorImg.className = "home-image";
+    interiorImg.src = interior;
+    interiorImg.alt = "An image of the interior of the Krusty Krab."
+    interiorImg.height = "500";
+    interiorImg.width = "750";
+
+    const midText = document.createElement("div");
+    midText.className = "home-text";
+    midText.textContent = "As the world's only source of the illustrious Krabby Patty, the Krusty Krab strives to further the culinary art of burger making."
+
+    const burgerImg = document.createElement("img");
+    burgerImg.className = "home-image";
+    burgerImg.src = burger;
+    burgerImg.alt = "An image of the renowned original Krabby Patty."
+    burgerImg.height = "500";
+    burgerImg.width = "750";
+
+    const bottomText = document.createElement("div");
+    bottomText.className = "home-text";
+    bottomText.textContent = "We hope you come visit!"
+
+    const crewImg = document.createElement("img");
+    crewImg.className = "home-image";
+    crewImg.src = crew;
+    crewImg.alt = "An image of the renowned original Krabby Patty."
+    crewImg.height = "500";
+    crewImg.width = "750";
+
+    home.appendChild(title);
+    home.appendChild(splashImg)
+    home.appendChild(introText);
+    home.appendChild(interiorImg);
+    home.appendChild(midText);
+    home.appendChild(burgerImg);
+    home.appendChild(bottomText);
+    home.appendChild(crewImg);
+
+
 
     return home;
 }
